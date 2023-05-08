ddbc76142e5d32fe3660ba08e2842b6403a4c9e0
BenjaminCruzado <b.cruzado01@ufromail.cl>
2023-05-08 18:12:13 -0400

    Subo las dos clases generadas con el Visual Paradigm

---
+++ src/main/java/org/example/CarroCompra.java
@@ -0,0 +1,20 @@
+package org.example;
+public class CarroCompra {
+	private int[][] productos;
+
+	public CarroCompra() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int calcularTotal() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int subTotal(int cant, int precio) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		throw new UnsupportedOperationException();
+	}
+}
